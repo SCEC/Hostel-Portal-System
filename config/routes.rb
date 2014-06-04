faacbec2b6b3c375f107fc5cd201657f52de455f
Philip Nunoo <ketay99@gmail.com>
2014-06-04 11:29:18 +0000

    Some styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ HPS::Application.routes.draw do
   get "welcome/index"
   get "welcome/contact"
   get "welcome/about"
+
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
